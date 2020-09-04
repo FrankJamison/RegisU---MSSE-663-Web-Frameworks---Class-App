dba47b03b2a353a0d8ae6e72d6cb54fc45e102ee
Frank Jamison <42881082+FrankJamison@users.noreply.github.com>
2020-09-03 20:26:37 -0700

    week 2 examples

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  title = 'Hero Dashboard';
+  subTitle = 'Rent a Hero';
+  isAuth: boolean;
+
+  budget = '7000000000';
+
+  heroes = [{
+    name: 'Captain Marvel',
+    rent: '2000000000'
+  }, { 
+    name: 'Spiderman', 
+    rent: 100000
+   }, {
+     name: 'Black Panther',
+     rent: '100000000'
+   }, {
+     name: 'Thor',
+     rent: 2000000000
+   }];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.isAuth = true;
+  }
+}
